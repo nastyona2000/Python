4c9e77536a6aedffbbc5510db7b0487071ac3a6d
nastyona2000 <148352888+nastyona2000@users.noreply.github.com>
2024-11-27 16:54:12 +0200

    Create hacker.py

---
+++ hacker.py
@@ -0,0 +1,17 @@
+#login = input("Enter your email: ")
+#password = input("Enter your password: ")
+right_login = "user228"
+right_password = 15 #з двох цифр
+
+for t in range(10):
+    for f in range(10):
+        pas = t*10 + f
+        print(pas)
+        if pas == right_password:
+            print("ACCESS GRANTED! Pass: ", pas);
+            break
+    if pas == right_password:
+            print("ACCESS GRANTED! Pass: ", pas);
+            break
+  
+    

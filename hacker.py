9b614796f0c6de774663aa80c24935461541d189
nastyona2000 <148352888+nastyona2000@users.noreply.github.com>
2024-12-04 17:00:54 +0200

    Update hacker.py

--- hacker.py
+++ hacker.py
@@ -1,17 +1,20 @@
-#login = input("Enter your email: ")
-#password = input("Enter your password: ")
+# login = input("Enter your email: ")
+# password = input("Enter your password: ")
 right_login = "user228"
-right_password = 15 #з двох цифр
+right_password = 125 #з двох цифр
 
-for t in range(10):
-    for f in range(10):
-        pas = t*10 + f
-        print(pas)
+for h in range(10):
+    for t in range(10):
+        for f in range(10):
+            pas = h*100 + t*10 + f
+            print(pas)
+            if pas == right_password:
+                print("ACCESS GRANTED! Pass: ", pas);
+                break
         if pas == right_password:
-            print("ACCESS GRANTED! Pass: ", pas);
-            break
+                print("ACCESS GRANTED! Pass: ", pas);
+                break
     if pas == right_password:
-            print("ACCESS GRANTED! Pass: ", pas);
-            break
+        break
   
     
